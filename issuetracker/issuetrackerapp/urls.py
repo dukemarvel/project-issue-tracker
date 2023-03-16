96031658b4290cf3613bfa491fa57e5ba1a1d03c
DukeMarvel <josephblackduke@icloud.com>
2023-03-16 16:39:47 +0100

    updated the urls.py

--- issuetracker/issuetrackerapp/urls.py
+++ issuetracker/issuetrackerapp/urls.py
@@ -1,5 +1,10 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 from .views import issue_list, issue_filter, issue_add, issue_edit, issue_delete
+from .api import IssueViewSet
+
+router = routers.DefaultRouter()
+router.register(r'issues', IssueViewSet)
 
 urlpatterns = [
     path('', issue_list, name='issue_list'),
@@ -7,4 +12,5 @@ urlpatterns = [
     path('add/', issue_add, name='issue_add'),
     path('edit/<int:pk>/', issue_edit, name='issue_edit'),
     path('delete/<int:pk>/', issue_delete, name='issue_delete'),
+    path('api/', include(router.urls)),
 ]
