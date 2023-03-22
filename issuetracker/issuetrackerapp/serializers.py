1542fc251f386934e0a1f4f1d5f397ab2d1b76d2
DukeMarvel <josephblackduke@icloud.com>
2023-03-22 13:09:08 +0100

    set up the api routers

--- issuetracker/issuetrackerapp/serializers.py
+++ issuetracker/issuetrackerapp/serializers.py
@@ -4,4 +4,4 @@ from .models import Issue
 class IssueSerializer(serializers.ModelSerializer):
     class Meta:
         model = Issue
-        fields = ['id', 'title', 'owner', 'status', 'creation_date', 'effort', 'estimated_completion_date']
+        fields = ['id', 'title', 'owner', 'status', 'created_date', 'effort_required', 'estimated_completion_date']
